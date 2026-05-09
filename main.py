c2a85d5c0a51ddde499cc0b29f281e0e4d6a9b88
mvsebastian <seba.online.tv@gmail.com>
2026-05-09 22:37:19 +0200

    Make port configurable via environment variable

--- main.py
+++ main.py
@@ -45,7 +45,8 @@ def toggle():
     return "DONE"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
